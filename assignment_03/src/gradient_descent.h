295a0af63d2c70344e58d35770ba42ed07936885
2026AIM1019 <2026aim1019@iitrpr.ac.in>
2026-08-19 15:30:52 +0530

    Removed Assignment 3

--- assignment_03/src/gradient_descent.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef GRADIENT_DESCENT_H
-#define GRADIENT_DESCENT_H
-#include "common.h"
-GDResult gradientDescent(const vector<double>& c, double x, double rate,
-                         double tolerance, int maxIterations);
-#endif
--- assignment_03/src/maxflow.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef MAXFLOW_H
-#define MAXFLOW_H
-#include "common.h"
-FlowResult maxflow(const Graph& graph);
-#endif
